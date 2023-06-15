b52c6f3b3b10e05961beb08c86cf4ad2492e1b90
Brak3 <gabrielvieira040@outlook.com>
2023-06-15 10:35:38 -0300

    feat: livros agora são salvos em um HASH e separados por categoria

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,15 +1,17 @@
 class Livro
   attr_accessor :valor
+  attr_reader :categoria
 
-  def initialize(autor, isbn = "1", numero_de_paginas, valor)
+  def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
     @autor = autor
     @isbn = isbn
     @numero_de_paginas = numero_de_paginas
     @valor = valor
+    @categoria = categoria
   end
 
   def to_s
-    "Autor: #{@autor}, Isbn: #{@isbn}, Numero de paginas: #{@numero_de_paginas}"
+    "Autor: #{@autor}, Isbn: #{@isbn}, Numero de paginas: #{@numero_de_paginas}, Categoria: #{categoria}"
   end
 
 end
