0c54fe827f4921ab0b6d9de0b7a0ddd8c44e2202
Brak3 <gabrielvieira040@outlook.com>
2023-06-20 11:38:44 -0300

    feat: diretorio dos arquivos midia e dvd adicionados

--- lib/loja_virtual.rb
+++ lib/loja_virtual.rb
@@ -1,3 +1,5 @@
+require File.expand_path("lib/midia")
+require File.expand_path("lib/dvd")
 require File.expand_path("lib/livro")
 require File.expand_path("lib/biblioteca")
 require File.expand_path("lib/relatorio")
