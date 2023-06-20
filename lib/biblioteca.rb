c9cf370c362b190e790537a98f9c1f3674c5f597
Brak3 <gabrielvieira040@outlook.com>
2023-06-20 18:54:30 -0300

    Breaking change: Classe biblioteca virou Module VendaFacil, com a classe Set

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -1,26 +1,28 @@
-class Biblioteca
-  attr_reader :livros
-  def initialize
+module Vendafacil
+
+  class Set
+    attr_reader :livros
+
+    def initialize
     @banco_de_arquivos = BancoDeArquivos.new
   end
 
-  def adiciona(livro)
-    salva(livro) do
-      livros << livro
+    def adiciona(livro)
+      salva(livro) do
+        livros << livro
     end
   end
 
-  def livros
-    @livros ||= @banco_de_arquivos.carrega
+    def livros
+      @livros ||= @banco_de_arquivos.carrega
+    end
+
+    def livros_por_categoria(categoria)
+      livros.select { |livro| livro.categoria == categoria }
+    end
+
+    def salva(livro)
+      @banco_de_arquivos.salva livro
+    end
   end
-
-  def livros_por_categoria(categoria)
-    livros.select { |livro| livro.categoria == categoria }
-  end
-
-  def salva(livro)
-    @banco_de_arquivos.salva livro
-  end
-
-
 end
