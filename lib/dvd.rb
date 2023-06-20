bc971017c8971088c0b6e0e3dd63f9723cb8837b
Brak3 <gabrielvieira040@outlook.com>
2023-06-20 14:32:23 -0300

    feat: Adicionado valor padrão de desconto igual a 10%

--- lib/dvd.rb
+++ lib/dvd.rb
@@ -3,10 +3,10 @@ class Dvd < Midia
   attr_reader :categoria
 
   def initialize(titulo, valor, categoria)
+    super()
     @titulo = titulo
     @valor = valor
     @categoria = categoria
-    @desconto = 0.1
   end
 
   def to_s
